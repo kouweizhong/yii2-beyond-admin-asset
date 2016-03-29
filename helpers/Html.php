3cad03030f3a3049eef64c6685420f4f23c4e76c
kriss <462679766@qq.com>
2016-03-29 10:27:03 +0800

    添加Html helper

---
+++ helpers/Html.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace kriss\beyond\helpers;
+
+use yii\helpers\ArrayHelper;
+
+class Html extends \yii\helpers\Html
+{
+    /**
+     * 让radio样式符合模板风格
+     * @param string $name
+     * @param bool $checked
+     * @param array $options
+     * @return string
+     */
+    public static function radio($name, $checked = false, $options = []) {
+        $label = ArrayHelper::remove($options, 'label');
+        $options['label'] = self::tag('span', $label, ['class' => 'text']);
+        return self::tag('div', parent::radio($name, $checked, $options), ['class' => 'radio']);
+    }
+}
