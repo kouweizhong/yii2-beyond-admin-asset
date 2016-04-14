01ae4e28c18c5c46e1c9c80bec045c84f142073c
kriss <462679766@qq.com>
2016-04-14 08:57:02 +0800

    添加checkbox的样式

--- helpers/Html.php
+++ helpers/Html.php
@@ -18,4 +18,17 @@ class Html extends \yii\helpers\Html
         $options['label'] = self::tag('span', $label, ['class' => 'text']);
         return self::tag('div', parent::radio($name, $checked, $options), ['class' => 'radio']);
     }
+
+    /**
+     * 让checkbox样式符合模板风格
+     * @param string $name
+     * @param bool $checked
+     * @param array $options
+     * @return string
+     */
+    public static function checkbox($name, $checked = false, $options = []) {
+        $label = ArrayHelper::remove($options, 'label');
+        $options['label'] = self::tag('span', $label, ['class' => 'text']);
+        return self::tag('div', parent::checkbox($name, $checked, $options), ['class' => 'checkbox']);
+    }
 }
